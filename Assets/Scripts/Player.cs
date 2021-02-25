eca1e804407e3349a66f79d789a9d72f1e2da0a6
dov36 <daniel.ggradinaru@gmail.com>
2021-02-25 21:59:56 +0200

    Create Player.cs

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Material defaultColor;
+    public Material selectedColor;
+
+    public string[] mana = new string[5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      this.GetComponent<MeshRenderer>().material = defaultColor;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
